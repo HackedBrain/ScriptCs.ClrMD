1db168ee47ec1d114e01295ccc0fbb57ca2832c5
Drew Marsh <drub0y@hotmail.com>
2013-05-11 23:20:24 -0700

    Adding ability to detatch from the process
    - Added the DetatchFromCurrentProcess method to ClrMdPack allowing you
    to eventually release the process after analyzing it.
    - Removing README/LICENSE from the csproj (should have already checked
    this in, missed)

--- src/ScriptCs.ClrMD/ClrMdPack.cs
+++ src/ScriptCs.ClrMD/ClrMdPack.cs
@@ -5,13 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Diagnostics.Runtime;
+using Microsoft.Diagnostics.Runtime.Interop;
 using ScriptCs.Contracts;
 
 namespace HackedBrain.ScriptCs.ClrMd
 {
 	public partial class ClrMdPack : IScriptPackContext
 	{
-		private const int DefaultAttachWaitTimeMilliseconds = 5000;		
+		private const int DefaultAttachWaitTimeMilliseconds = 5000;
+
+		private DataTarget currentDataTarget;
 		
 		public ClrRuntime AttachToProcess(string processName)
 		{
@@ -41,19 +44,25 @@ namespace HackedBrain.ScriptCs.ClrMd
 
 		public ClrRuntime AttachToProcess(int processId, int attachWaitTimeMilliseconds)
 		{
-			DataTarget dataTarget = DataTarget.AttachToProcess(processId, (uint)attachWaitTimeMilliseconds);
+			if(this.currentDataTarget != null)
+			{
+				// TODO: find way to put process id/name in this exception message
+				throw new InvalidOperationException(string.Format("Already attached to a process, use DetatchFromCurrentProcess first."));
+			}
+			
+			this.currentDataTarget = DataTarget.AttachToProcess(processId, (uint)attachWaitTimeMilliseconds);
 
 			// Make sure the CLR is even found
-			if(dataTarget.ClrVersions.Count == 0)
+			if(this.currentDataTarget.ClrVersions.Count == 0)
 			{
 				throw new InvalidOperationException(string.Format("The specified process ({0}) does not appear to have the CLR loaded in it.", processId));
 			}
-			else if(dataTarget.ClrVersions.Count > 1)
+			else if(this.currentDataTarget.ClrVersions.Count > 1)
 			{
 				// REVISIT: what happens if there's multiple ClrVersions?
 			}
 
-			ClrInfo clrInfo = dataTarget.ClrVersions[0];		
+			ClrInfo clrInfo = this.currentDataTarget.ClrVersions[0];		
 			
 			string dacLocation = clrInfo.TryGetDacLocation();
 
@@ -63,7 +72,27 @@ namespace HackedBrain.ScriptCs.ClrMd
 				throw new InvalidOperationException(string.Format("Unable to locate the DAC for the target version of the CLR runtime ({1}).", processId, clrInfo.Version));
 			}
 
-			return dataTarget.CreateRuntime(dacLocation);
+			// Make sure that if we're unloaded we don't kill off the process
+			this.currentDataTarget.DebuggerInterface.SetProcessOptions(DEBUG_PROCESS.DETACH_ON_EXIT);
+
+			return this.currentDataTarget.CreateRuntime(dacLocation);
+		}
+
+		public void DetatchFromCurrentProcess()
+		{
+			this.EnsureAttachedToProcess();
+			
+			this.currentDataTarget.DebuggerInterface.DetachProcesses();
+			this.currentDataTarget.Dispose();
+			this.currentDataTarget = null;
+		}
+
+		private void EnsureAttachedToProcess()
+		{
+			if(this.currentDataTarget == null)
+			{
+				throw new InvalidOperationException("Not attached to any process right now.");
+			}
 		}
 	}
 }
