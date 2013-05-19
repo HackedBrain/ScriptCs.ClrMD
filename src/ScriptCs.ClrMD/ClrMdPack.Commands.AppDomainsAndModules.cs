a6b20517f58a6fd3f8c931e4fe4e2728472cdeb9
Drew Marsh <drub0y@hotmail.com>
2013-05-18 22:34:50 -0700

    Implementing DumpAppDomains
    Closes #6.

--- src/ScriptCs.ClrMD/ClrMdPack.Commands.AppDomainsAndModules.cs
+++ src/ScriptCs.ClrMD/ClrMdPack.Commands.AppDomainsAndModules.cs
@@ -11,7 +11,16 @@ namespace HackedBrain.ScriptCs.ClrMd
 	{
 		public void DumpAppDomains()
 		{
-			foreach(ClrAppDomain appDomain in this)
+			foreach(ClrAppDomain appDomain in this.currentClrRuntime.AppDomains)
+			{
+				this.outputWriter.WriteLine("Id: {0}", appDomain.Id);
+				this.outputWriter.WriteLine("Name: {0}", appDomain.Name);
+				this.outputWriter.WriteLine("Address: {0}", appDomain.Address);
+				this.outputWriter.WriteLine("AppBase: {0}", appDomain.AppBase);
+				this.outputWriter.WriteLine("Config File: {0}", appDomain.ConfigurationFile);
+
+				this.outputWriter.WriteLineSeparator();
+			}
 		}
 	}
 }
